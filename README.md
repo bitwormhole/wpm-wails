d1cc596f5a9782398967c6996e3c6ea774f4cc0b
MEZ <mez@bitwormhole.com>
2023-02-28 16:29:07 +0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # wpm-wails
 采用 wails 实现的 wpm app
+
+
+编译或开发之前需要安装 wails。参考：(https://wails.io/zh-Hans/docs/gettingstarted/installation)
+
+## 编译
+    wails build
+
+## 运行开发版
+    wails dev
--- main.go
+++ main.go
@@ -14,12 +14,6 @@ func main() {
 	server := &myServer{context: ctx}
 	w := &myWails{context: ctx}
 
-	// vlog.Trace(client, server, w)
-	// x, ok := os.LookupEnv("WPM_WAILS_SKIP_RUN")
-	// if x == "1" && ok {
-	// 	return
-	// }
-
 	client.Run()
 	server.Start()
 	w.Run()
